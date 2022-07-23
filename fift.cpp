d26ce16e323a65bc2b9dcbf2baeb391b520f2904
XSR <ogienognisty@gmail.com>
2022-07-23 15:01:18 +0300

    fixes to pass with WASM

--- fift.cpp
+++ fift.cpp
@@ -3,14 +3,16 @@
 #include "crypto/fift/Fift.h"
 
 #include "lib.hpp"
+#include "fifsrc.hpp"
 
 std::string interpret(const std::string & src) {
   std::ostringstream output;
 
-  std::string current_dir = td::realpath(".").move_as_ok();
-  std::string lib_dir = td::realpath("./lib/").move_as_ok();
+  // std::string current_dir = td::realpath(".").move_as_ok();
+  std::string current_dir = "";
+  // std::string lib_dir = td::realpath("./lib/").move_as_ok();
   fift::SourceLookup source_lookup = fift::SourceLookup(std::make_unique<fift::OsFileLoader>());
-  source_lookup.add_include_path(lib_dir);
+  // source_lookup.add_include_path(lib_dir);
   fift::Dictionary dictionary;
   fift::init_words_common(dictionary);
   fift::init_words_vm(dictionary, true);  // enable vm debug
@@ -22,7 +24,8 @@ std::string interpret(const std::string & src) {
   ctx.output_stream = &output;
   ctx.error_stream = &std::cerr;
 
-  auto fift_lib_stream = std::make_unique<std::stringstream>(std::move(source_lookup.lookup_source("Fift.fif", lib_dir).ok().data));
+  // auto fift_lib_stream = std::make_unique<std::stringstream>(std::move(source_lookup.lookup_source("Fift.fif", lib_dir).ok().data));
+  auto fift_lib_stream = std::make_unique<std::stringstream>(std::move(fift_fif_src));
   ctx.enter_ctx("Fift.fif", current_dir, std::move(fift_lib_stream));
   ctx.run(td::make_ref<fift::InterpretCont>());
 
--- test.cpp
+++ test.cpp
@@ -4,14 +4,20 @@
 
 #include "lib.hpp"
 
+std::string func_src = "() recv_internal(int msg_value, cell in_msg_full, slice in_msg_body) impure { \
+  throw(0xffff); \
+}";
+
 int main() {
-  std::stringstream stringstream;
-  stringstream << std::ifstream("./test.fc").rdbuf();
+  // std::stringstream stringstream;
+  // stringstream << std::ifstream("./test.fc").rdbuf();
 
   std::cout << "test func source:\n===================\n";
-  std::cout << stringstream.str() << std::endl;
+  // std::cout << stringstream.str() << std::endl;
+  std::cout << func_src << std::endl;
 
-  auto generated = generate(stringstream.str());
+  // auto generated = generate(stringstream.str());
+  auto generated = generate(func_src);
 
   std::cout << "\ngenerated fift:\n==================\n";
   std::cout << generated << std::endl;
